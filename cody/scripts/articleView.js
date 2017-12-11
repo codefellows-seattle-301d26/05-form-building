aa9708057ab9414283fcce05e855406af747f493
Cody <williwonka14@gmail.com>
2017-12-10 23:29:26 -0800

    fixing things in js

--- cody/scripts/articleView.js
+++ cody/scripts/articleView.js
@@ -77,7 +77,7 @@ articleView.setTeasers = () => {
 // It is called on the new.html page. To display the content.
 articleView.initNewArticlePage = () => {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-  $('.tab-content').show()
+  $('.tab-content').show();
 
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
@@ -91,10 +91,10 @@ articleView.initNewArticlePage = () => {
 };
 
 articleView.create = () => {
-  let article
+  let article;
   // TODO: Set up a variable to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
-  $('#articles').empty()
+  $('#articles').empty();
 
   // TODO: Instantiate an article based on what's in the form fields:
   article = new Article({
@@ -104,26 +104,22 @@ articleView.create = () => {
     category: $('#article-category').val(),
     body: $('#article-body').val(),
     publishedOn: $('#article-pubdate:checked').length ? new Date() : null,
-  })
+  });
 
   // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
-  $('#articles').append(article.toHtml())
+  $('#articles').append(article.toHtml());
 
   // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
   $('pre code').each((i, block) => hljs.highlightBlock(block));
 
-  // $('pre code').each(function(i, block) {
-  //   hljs.highlightBlock(block)
-  // })
 
   // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
-  $('#article-export').show().find('#article-json').val(JSON.stringify(article))
+  $('#article-export').show().find('#article-json').val(JSON.stringify(article));
 };
 
 // COMMENT: Where is this function called? Why?
-// It is called on the new.html page. To display the content.
+// It is called on the index.html page. To display the content.
 articleView.initIndexPage = () => {
-  // articles.forEach(article => $('#articles').append(article.toHtml()));
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
