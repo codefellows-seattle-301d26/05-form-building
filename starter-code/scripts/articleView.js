b51c82ac3a37cf8806db9a9cbc1949b281a0e62b
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-12-22 17:52:43 -0800

    Everything functioning and working bueatifully, dynamnically, and gracefully!

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -77,34 +77,19 @@ articleView.setTeasers = () => {
 // COMMENT: Where is this function called? Why?
 // This function is called in the new.html to dynamically render new article content to the DOM
 articleView.initNewArticlePage = () => {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-
-('tab-content').show();
-
-  // TODO: The new articles we create will be copy/pasted into our source data file.
-  // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
-  //I added one line of code  
+  ('tab-content').show();  
   ('#article-export').hide();
   $('#article-json').on('focus', function(){
     this.select();
   });
-//the above says 'on focus "when the mouse is focused" make it easy to copy/paste (user interface plug)
-  // TODO: Add an event handler to update the preview and the export field if any inputs change.
-//deligation i am adding line below
-$('#article-form').on('change', 'input, textarea', articleView.create)
-};
+
+  $('#article-form').on('change', 'input, textarea', articleView.create)
+  };
 
 articleView.create = () => {
-  // TODO: Set up a variable to hold the new article we are creating.
   let article
-  //the below code clears clears the #articles ID so we ca update preview
-  // Clear out the #articles element, so we can put in the updated preview
   $('#articles').empty()
 
-  
-
-  // TODO: Instantiate an article based on what's in the form fields:
-  //DONE BY ME
 article = new Article({
   author: $('#article-autor').val(),
   authorUrl: $('#article-authorUrl').val(),
@@ -112,18 +97,10 @@ article = new Article({
   category: $('#article-category').val(),
   body: $('#article-body').val(),
   publishedOn: $('#article-pubdate:checked').length? new Date () : null,
-  //checks for true or false and prints date stamp based on check box 'published' event listener
 })
 
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
-  //ME WORKING ON.. DONE
-
 $('#articles').append(article.tHhtml())
-
-  // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
-  $('pre code').each();((i, block) => hljs.highlightBlock(block));
-
-  // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+$('pre code').each();((i, block) => hljs.highlightBlock(block));
 $('#article-export').show().find('#article-json').val(Json.stringify(article))
 };
 // COMMENT: Where is this function called? Why?
